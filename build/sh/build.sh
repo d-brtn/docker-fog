18c733674ad30f6ed82a2b4ec14041682c2a0a61
d-brtn <20693977+d-brtn@users.noreply.github.com>
2023-08-30 18:39:35 +0000

    tftp_debugging

--- build/sh/build.sh
+++ build/sh/build.sh
@@ -1,5 +1,6 @@
 echo "|||_DYNAMICALLY PULLING GIT ARCHIVES_|||"
 #UPDATE GIT SOURCES RECURSIVELY
-ls | xargs -P10 -I{} git -C {} puecho "|||_TRYING DOCKER COMPOSE_|||"
+ls | xargs -P10 -I{} git -C {}
+""|||_TRYING DOCKER COMPOSE_|||"
 docker compose up --build -d
 
